55c7bbb27983056175397e69f0da1a34ca8fece9
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-07-11 21:04:05 +0300

    fix:querying data in mysql error fixed

--- index.js
+++ index.js
@@ -4,21 +4,33 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
 
-const db = mysql.createPool({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "crud_materiel"
+    database: "affecteo"
 });
+
+connection.connect((err) => {
+    if(err){
+        return console.log(err)
+    }
+    console.log('Connected to Mysql server')
+})
+
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extend: true}));
 
 app.get("/api/get", (req, res) => {
-    const sqlGet = "SELECT * FROM crud_materiel";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
+    const sqlGet = `SELECT * FROM Lieu`;
+
+    connection.query(sqlGet, (error, result) => {
+        console.log(error)
+        console.log(result)
+        if (error) return res.status(400).json({status:'fail',message:error.message})
+        res.status(200).json(result);
     });
 });
 
